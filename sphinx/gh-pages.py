d5d9b0874680873a79bef566cbaac00b3cb0ad15
Walt Woods <woodswalben@gmail.com>
2016-07-26 09:52:46 -0700

    Automated gh-pages.py script for publishing Github docs

--- sphinx/gh-pages.py
+++ sphinx/gh-pages.py
@@ -3,13 +3,7 @@
 import click
 import os
 from subprocess import CalledProcessError, check_call, check_output
-
-def _checkBranch():
-    try:
-        check_call([ 'git', 'rev-parse', '--verify', 'gh-pages' ])
-    except CalledProcessError:
-        # Need to make the branch
-
+import sys
 
 @click.command()
 @click.argument('html_path')
@@ -28,11 +22,63 @@ def main(html_path):
     if not os.path.lexists(os.path.join(html_path, 'index.html')):
         raise ValueError("Bad HTML path: no index.html file")
 
-    # Add a .nojekyll file
-    with open(os.path.join(html_path, '.nojekyll'), 'w') as f:
-        pass
+    # Ensure in git repo
+    gitBase = os.path.abspath(os.getcwd())
+    while not os.path.lexists(os.path.join(gitBase, '.git')):
+        old = gitBase
+        gitBase = os.path.dirname(gitBase)
+        if gitBase == old:
+            sys.stderr.write("This does not appear to be within a git "
+                    "repository; this tool is for Github.\n")
+            sys.exit(1)
 
-    print("HTML Path: {}".format(html_path))
+    nojek = os.path.join(html_path, '.nojekyll')
+    if not os.path.lexists(nojek):
+        # Add a .nojekyll file
+        with open(nojek, 'w') as f:
+            pass
+
+    # Ensure that there is nothing added to the index, ready to be committed,
+    # as this script might trigger a commit.
+    try:
+        check_call([ 'git', 'diff-index', '--quiet', '--cached', 'HEAD' ])
+    except CalledProcessError:
+        sys.stderr.write("*"*79 + "\n")
+        sys.stderr.write("gh-pages can only be made when there is nothing "
+                "currently 'git add'ed to the index; as gh-pages might "
+                "trigger a commit, anything in the index would be committed "
+                "under an irrelevant message.\n")
+        sys.exit(1)
+
+    if check_output([ 'git', 'status', '--porcelain', html_path ]).strip():
+        # Commit the built documentation
+        check_call([ 'git', 'add', '-fA', html_path ])
+        check_call([ 'git', 'commit', '-m',
+                'Updating documentation for gh-pages' ])
+
+    print("Please open in browser: file://{}".format(os.path.abspath(
+            os.path.join(html_path, 'index.html'))))
+    print("If everything looks OK, entering [Y] will update the gh-pages "
+            "branch at origin.")
+    while True:
+        r = input("Proceed? [Y/n] ")
+        if r.lower() == 'n':
+            print("Aborting.")
+            sys.exit(1)
+        elif r == 'Y':
+            print("Continuing.")
+            break
+
+    subtree = os.path.relpath(os.path.abspath(html_path), gitBase)
+    old = os.getcwd()
+    try:
+        os.chdir(gitBase)
+        ghpages = check_output([ 'git', 'subtree', 'push', '--prefix',
+                subtree, 'origin', 'gh-pages' ]).strip().decode('utf-8')
+    finally:
+        os.chdir(old)
+    print("Done.")
+
 
 if __name__ == '__main__':
     main()
